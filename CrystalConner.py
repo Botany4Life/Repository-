866cf93c2c88b68ef99bddd7d45aaf25952b3877
Botany4Life <93221274+Botany4Life@users.noreply.github.com>
2023-03-09 13:24:03 -0500

    ROugh draft

---
+++ CrystalConner.py
@@ -0,0 +1,48 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+menu = True
+
+
+def main():
+
+
+
+
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+    while menu:
+        if option == 1:
+            option = int(input("please enter an option: "))
+            password = int(input("Please enter your password to encode: "))
+            new_password = int(input('Your password has been encodded and stored!'))
+        elif option == 2:
+            pass
+        elif option ==3:
+            pass
+
+
+
+
+if __name__ == '__main__"':
+    main()
